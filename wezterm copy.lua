907599c5d7ad77f8fb07a98ffd93ca09f1f0ad1b
Tony Guardia <afg@guardia.org>
2023-09-08 01:49:21 +0100

    Add files via upload

---
+++ wezterm copy.lua
@@ -0,0 +1,11 @@
+local wezterm = require("wezterm")
+local config = {}
+
+config.force_reverse_video_cursor = true
+config.window_background_opacity = 0.8
+config.window_decorations = "RESIZE"
+config.hide_tab_bar_if_only_one_tab = true
+
+config.color_scheme = "Kanagawa (Gogh)"
+
+return config
